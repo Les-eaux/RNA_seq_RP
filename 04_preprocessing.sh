98040a89e31e4ebb0ca51ed25de4c15844c05d6b
Leo Wuetschert <lwuetschert@binfservms01.unibe.ch>
2023-11-14 17:10:24 +0100

    update 04 code + fastQC 2

--- 04_preprocessing.sh
+++ 04_preprocessing.sh
@@ -3,7 +3,7 @@ READS_DIR=/data/users/lwuetschert/rnaseq_course/RNA_fastq
 
 #SBATCH --cpus-per-task=4    #cores
 #SBATCH --time=02:00:00       #time limit
-#SBATCH --mem-per-cpu=1000M     #requested memory
+#SBATCH --mem-per-cpu=2000M     #requested memory
 #SBATCH --output=/data/users/lwuetschert/rnaseq_course/fastqc_output_%j.o         #output
 #SBATCH --error=/data/users/lwuetschert/rnaseq_course/fastqc_error_%j.e          #error
 #SBATCH --job-name=rnaseq_RP_fastqc     #job name
@@ -13,15 +13,25 @@ cd $READS_DIR
 
 module load UHTS/Quality_control/cutadapt/2.5
 
-#clip 3' adapter
-cutadapt -j 4 -q 25 -a CTGTAGGCACCATCAAT -m 25 --discard-untrimmed -o RPF_KO_Rep1_clpd.fastq.gz RPF_KO_Rep1.fastq.gz 
-cutadapt -j 4 -q 25 -a CTGTAGGCACCATCAAT -m 25 --discard-untrimmed -o RPF_KO_Rep2_clpd.fastq.gz RPF_KO_Rep2.fastq.gz 
-cutadapt -j 4 -q 25 -a CTGTAGGCACCATCAAT -m 25 --discard-untrimmed -o RPF_WT_Rep1_clpd.fastq.gz RPF_WT_Rep1.fastq.gz 
-cutadapt -j 4 -q 25 -a CTGTAGGCACCATCAAT -m 25 --discard-untrimmed -o RPF_WT_Rep2_clpd.fastq.gz RPF_WT_Rep2.fastq.gz 
+# Clip 3' adapter
+for x in $(ls -d *.fastq.gz); do echo ${x}; \
+cutadapt \
+-j 4 \
+-a CTGTAGGCACCATCAAT \
+-q 25 \
+--minimum-length 25 \
+--discard-untrimmed \
+-o $(basename ${x} .fastq.gz)_clpd.fastq.gz \
+${x} 1> $(basename ${x} .fastq.gz)_clpd_log.txt; done
 
-#trim 4nt
-cutadapt -j 4 -q 25 -u -4 -m 25 --discard-untrimmed -o RPF_KO_Rep1_clpd_tr.fastq.gz  RPF_KO_Rep1_clpd.fastq.gz 
-cutadapt -j 4 -q 25 -u -4 -m 25 --discard-untrimmed -o RPF_KO_Rep2_clpd_tr.fastq.gz  RPF_KO_Rep2_clpd.fastq.gz 
-cutadapt -j 4 -q 25 -u -4 -m 25 --discard-untrimmed -o RPF_WT_Rep1_clpd_tr.fastq.gz  RPF_WT_Rep1_clpd.fastq.gz 
-cutadapt -j 4 -q 25 -u -4 -m 25 --discard-untrimmed -o RPF_WT_Rep2_clpd_tr.fastq.gz  RPF_WT_Rep2_clpd.fastq.gz
+
+# Trim 4 nt (randomized bases) from the 3' end
+for x in $(ls -d *_clpd.fastq.gz); do echo ${x}; \
+cutadapt \
+-j 4 \
+-q 25 \
+--cut -4 \
+--minimum-length 25 \
+-o $(basename ${x} .fastq.gz)_tr.fastq.gz \
+${x} 1> $(basename ${x} .fastq.gz)_tr_log.txt; done
 
